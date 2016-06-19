135fc10eb398574cb8550f13f5e2ac00f3973c3a
cealvarez <cealvarez@uc.cl>
2016-06-19 01:38:15 -0400

    arreglando bot

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -36,6 +36,31 @@ class ApisController < ApplicationController
       return texto
   end
 
+  def juegos(game)
+    url = URI("https://videogamesrating.p.mashape.com/get.php?count=5&game="+game)
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(url)
+    request["x-mashape-key"] = 'zfEvpNOcb8mshz7Xeco6wIN9RPdBp160CByjsn6ct68ijin7So'
+    request["accept"] = 'application/json'
+    request["cache-control"] = 'no-cache'
+    request["postman-token"] = '88bc0b58-c8ce-a7ca-db7c-932eaa70cbc9'
+    response = http.request(request)
+    @oc_array = JSON.parse(response.body)
+    texto = ''
+    @oc_array.each do |game|
+      indice = game['short_description'].index('-')
+      fecha = game['short_description'][0 , indice - 1]
+      texto += 'Título: ' + game['title'] + "\ncalificación: " + game['score'] + "\nfecha de lanzamiento: " + fecha + "\nDisponible en: "
+      game['platforms'].each do |key, value|
+        texto += value + ' '
+      end
+      texto += "\n\n"
+    end
+    puts texto
+  end
+
   # GET /apis/1
   # GET /apis/1.json
 =begin
@@ -109,7 +134,15 @@ class ApisController < ApplicationController
           client.message channel: data['channel'], text: "<@#{data['user']}>, " + consultar(ciudad)
         end
 
-
+      when /(((info|informacion|información|detalles|calificación|calificacion|saber)(|.*juego)))/ then
+        indice = data['text'].rindex('juego ')
+        if indice.nil?
+          indice = 0
+          client.message channel: data['channel'], text: "<@#{data['user']}> quieres información de algún JUEGO en particular?"
+        else
+          j = data['text'][indice + 6, data['text'].length]
+          client.message channel: data['channel'], text: "<@#{data['user']}>, " + juegos(j)
+        end
         
       when /^bot.([j][i]){2,}/ then
         client.message channel: data['channel'], text: "hahaha <@#{data['user']}>"
