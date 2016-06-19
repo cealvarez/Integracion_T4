5b1bbbbfd5ffa95e6dfd72be9ef6d6ae74800960
cealvarez <cealvarez@uc.cl>
2016-06-18 22:51:13 -0400

    arreglando comportamiento bot

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -112,6 +112,9 @@ class ApisController < ApplicationController
         client.message channel: data['channel'], text: "hahaha <@#{data['user']}>"
       when /^bot/ then
         client.message channel: data['channel'], text: "Sorry <@#{data['user']}>, what?"
+      when /(adios|chao|hasta pronto)/ then
+        client.message channel: data['channel'], text: "Gracias por preferirme"
+        client.stop!
       end
     end
 
